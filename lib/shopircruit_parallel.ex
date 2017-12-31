68bc7302f77b35db357a1697eb73b29427297414
Logan Martel <logan.martel@outlook.com>
2017-12-31 18:41:00 -0500

    Credit pmap to apropriate author

--- lib/shopircruit_parallel.ex
+++ lib/shopircruit_parallel.ex
@@ -1,4 +1,9 @@
+# Courtesy of: http://nathanmlong.com/2014/07/pmap-in-elixir/
 defmodule Shopircruit.Parallel do
+  @doc """
+      iex> Parallel.map([1,2,3], &(&1*2))
+      [2,4,6]
+  """
   def pmap(collection, function) do
     collection
     |> Enum.map(&Task.async(fn -> function.(&1) end))
